4e1674bbf8286ed0d5801fcab8755bc8026cf37b
salman <salman.khan@myglamm.com>
2019-12-08 17:50:20 +0530

    added new functionality to select dropdown

--- src/app/select/select.component.ts
+++ src/app/select/select.component.ts
@@ -10,6 +10,12 @@ export class SelectComponent implements OnInit {
   selectedColor:any ='Colours';
   searchText:any;
   itemsCopy: any;
+  isOptionChecked :any = {
+    selectAll : false
+  };
+  selectOption:any=[];
+  result: string;
+  showDropDown:boolean = false
   constructor() { }
 
   ngOnInit() {
@@ -21,17 +27,46 @@ export class SelectComponent implements OnInit {
     console.log(this.searchText);
     
     this.options = this.itemsCopy.filter((tag)=> {
-      console.log(tag);
         if(tag.name){
           return tag.title.indexOf(term) >= 0;
         }else{
+          if(tag.indexOf(term)>=0){
+            console.log(tag);
+          }
           return tag.indexOf(term) >= 0;
         }
     }); 
 }
 selectAll(){
   console.log('test');
+  this.isOptionChecked.selectAll = !this.isOptionChecked.selectAll;
   
+}
+
+checkedBoxClicked(i){
+  console.log(i);
+  this.isOptionChecked[i] = ! this.isOptionChecked[i];
+  if(this.isOptionChecked[i]){
+    this.selectOption.push(this.itemsCopy[i]);
+  }else{
+    // remove selected element from array
+    this.selectOption.splice(i,1);
+  }
+}
+submit(){
+  this.showDropDown = false;
+  console.log(this.selectOption);
+  
+  this.result= this.selectOption.join();
+}
+
+clearResult(){
+  this.result = null;
+  this.isOptionChecked = {}
+}
+
+selectClick(){
+  this.showDropDown = !this.showDropDown;
 }
 
 }
